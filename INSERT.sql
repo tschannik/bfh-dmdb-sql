9f03f19b87cac5654f1395577cadf28ca2079149
Yannik Zimmermann <yannik.z@me.com>
2020-12-30 17:33:59 +0100

    updates scripts

--- INSERT.sql
+++ INSERT.sql
@@ -2,31 +2,101 @@
 INSERT INTO
     GEHALTSGRUPPE (GID, GEH)
 VALUES
-    (1, 'Lohngruppe 1'),
-    (2, 'Lohngruppe 2'),
-    (3, 'Lohngruppe 3'),
-    (4, 'Lohngruppe 4'),
-    (5, 'Lohngruppe 5'),
-    (6, 'Lohngruppe 6'),
-    (7, 'Lohngruppe 7'),
-    (8, 'Lohngruppe 8'),
-    (9, 'Lohngruppe 9'),
-    (10, 'Lohngruppe 10');
+    (10, 'Lohngruppe 1'),
+    (20, 'Lohngruppe 2'),
+    (30, 'Lohngruppe 3'),
+    (40, 'Lohngruppe 4'),
+    (50, 'Lohngruppe 5'),
+    (60, 'Lohngruppe 6'),
+    (70, 'Lohngruppe 7'),
+    (80, 'Lohngruppe 8'),
+    (90, 'Lohngruppe 9'),
+    (99, 'Lohngruppe 10');
 
 /* Abteilungen */
 INSERT INTO
     ABTEILUNG (
         AID,
         Bezeichnung,
-        Kurzbezeichnung,
-        AbteilungsleiterID
+        Kurzbezeichnung
     )
 VALUES
-    (1, 'Human Resources', 'HR', 1001),
-    (2, 'Information Technologies', 'IT', 1000),
-    (3, 'Mitarbeiter', 'MA', 1004),
-    (4, 'Konzernleitung', 'KL', 1005),
-    (5, 'Extern', 'EXT', 1010);
+    (1, 'Human Resources', 'HR'),
+    (2, 'Information Technologies', 'IT'),
+    (3, 'Mitarbeiter', 'MA'),
+    (4, 'Konzernleitung', 'KL'),
+    (5, 'Extern', 'EXT');
+
+/* Bonus */
+INSERT INTO
+    BONUS (
+        BID,
+        Bonus_Prozent,
+        ErhaltAbAnstellungszeit
+    )
+VALUES
+    (1, -10, 1),
+    (2, -5, 2),
+    (3, 0, 3),
+    (4, 5, 5),
+    (5, 10, 7);
+
+/* Mitarbeiter */
+INSERT INTO
+    MITARBEITER (
+        PID,
+        Name,
+        Vorname,
+        Eintrittsdatum,
+        AbteilungsID,
+        GehaltsgruppeID,
+        StellvertreterID,
+        BonusID
+    )
+VALUES
+    (
+        1000,
+        'Radi',
+        'Karl',
+        '2020-10-01',
+        1,
+        10,
+        1000,
+        BONUSID
+    ),
+    (1001, 'Lette', 'Adi', '2020-10-01', 1, 20, NULL),
+    (1002, 'Theke', 'Andi', '2020-10-01', 2, 30, 1001),
+    (1003, 'Nass', 'Anna', '2020-10-01', 3, 40, NULL),
+    (1004, 'Gator', 'Alli', '2020-10-01', 2, 50, NULL),
+    (
+        1005,
+        'Schweis',
+        'Axel',
+        '2020-10-01',
+        3,
+        60,
+        NULL
+    ),
+    (
+        1006,
+        'Silie',
+        'Peter',
+        '2020-10-01',
+        4,
+        70,
+        NULL
+    ),
+    (1007, 'Uhr', 'Klaus', '2020-10-01', 5, 80, NULL),
+    (
+        1008,
+        'Steinfeger',
+        'Shawn',
+        '2020-10-01',
+        3,
+        10,
+        NULL
+    ),
+    (1009, 'Lung', 'Sam', '2020-10-01', 1, 20, NULL);
 
 /* Projekte */
 INSERT INTO
@@ -35,7 +105,8 @@ INSERT INTO
         Bezeichnung,
         Startdatum,
         Enddatum,
-        ParentProjektID
+        ParentProjektID,
+        ProjektleiterID
     )
 VALUES
     (
@@ -43,60 +114,51 @@ VALUES
         'Backup des Internets erstellen',
         '2020-10-20',
         '2021-10-20',
-        NULL
+        NULL,
+        1000
     ),
     (
         2,
         'Neuinstallation Heizungsanlagen aufgrund von Explosion nach Verwendung des falschen Schmiermittels',
         '2020-10-20',
         '2021-10-20',
-        NULL
+        NULL,
+        1001
     ),
     (
         3,
         'KI welche aufgrund der aktuellen Gefühlslage die perfekte Netflix-Serie vorschlägt',
         '2020-10-20',
         '2021-10-20',
-        NULL
+        NULL,
+        1000
     ),
     (
         4,
         'Einführung weltweit einheitlicher Steckdosen',
         '2020-10-20',
         '2021-10-20',
-        NULL
+        NULL,
+        1002
     ),
     (
         5,
         'Verlohrene Nadeln im Heuhaufen suchen',
         '2020-10-20',
         '2021-10-20',
-        NULL
+        NULL,
+        1003
     ),
     (
         6,
         'Schäfchen zählen',
         '2020-10-20',
         '2021-10-20',
-        NULL
+        NULL,
+        1004
     );
 
-/* Mitarbeiter */
-INSERT INTO
-    MITARBEITER (PID, Name, Vorname, Eintrittsdatum)
-VALUES
-    (1000, 'Radi', 'Karl', '2020-10-01'),
-    (1001, 'Lette', 'Adi', '2020-10-01'),
-    (1002, 'Theke', 'Andi', '2020-10-01'),
-    (1003, 'Nass', 'Anna', '2020-10-01'),
-    (1004, 'Gator', 'Alli', '2020-10-01'),
-    (1005, 'Schweis', 'Axel', '2020-10-01'),
-    (1006, 'Silie', 'Peter', '2020-10-01'),
-    (1007, 'Uhr', 'Klaus', '2020-10-01'),
-    (1008, 'Steinfeger', 'Shawn', '2020-10-01'),
-    (1009, 'Lung', 'Sam', '2020-10-01');
-
-/* Mitarbeiter */
+/* Projekt Mitarbeiter */
 INSERT INTO
     PROJEKT_MITARBEITER (
         MitarbeiterID,
@@ -115,7 +177,17 @@ VALUES
     (1006, 5, 100, '2020-10-01', '2020-11-01'),
     (1007, 6, 100, '2020-10-01', '2020-11-01'),
     (1008, 6, 100, '2020-10-01', '2020-11-01'),
-    (1009, 7, 100, '2020-10-01', '2020-11-01');
+    (1009, 5, 100, '2020-10-01', '2020-11-01');
+
+/* Abteilungsleiter */
+INSERT INTO
+    ABTEILUNGSLEITER(ATID, MitarbeiterID, AbteilungsID)
+VALUES
+    (1, 1000, 1),
+    (2, 1002, 2),
+    (3, 1003, 3),
+    (4, 1004, 4),
+    (5, 1005, 5);
 
 -- INSERT INTO
 --     MITARBEITER (PID, Name, Vorname, Eintrittsdatum, Bonus)
